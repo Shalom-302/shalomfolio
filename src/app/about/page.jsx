f68daaa52aca53f5e9114293e4a5b3808ac21e51
Shalom-302 <shalomtehe219@gmail.com>
2024-06-01 11:51:03 +0000

    profile picture

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -31,7 +31,7 @@ const AboutPage = () => {
           <div className="flex flex-col gap-12 justify-center">
             {/* BIOGRAPHY IMAGE */}
             <Image
-              src=""
+              src="/shalom.jpg"
               alt=""
               width={112}
               height={112}
